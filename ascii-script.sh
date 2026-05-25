bf130be09ca2a99fde96095f95a59b6a223cd226
mayurnikam266@gmail.com <mayurnikam266@gmail.com>
2026-05-25 18:37:11 +0530

    Add ASCII art shell script ascii-script.sh

---
+++ ascii-script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover; I am a DRAGON... RAWR!" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
+
+# by adding this you can reduce complex and too much steps 
